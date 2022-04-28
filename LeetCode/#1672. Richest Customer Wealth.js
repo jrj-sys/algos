981e14745b5227f3ee9f1fbdad54d00338dcbfe2
jrj-sys <jamesjamel99@gmail.com>
2022-04-27 23:59:17 -0500

    #1672 richest customer wealth

---
+++ LeetCode/#1672. Richest Customer Wealth.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+
+// You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. 
+// Return the wealth that the richest customer has.
+// A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+ 
+
+ const maximumWealth = accounts => {
+    let result = [];
+    
+    for (let i = 0; i < accounts.length; i++) {
+            result.push(accounts[i].reduce((p, c) => p + c));
+    }    
+    result.sort((a, b) => b - a)
+    return result.slice(0, 1);
+};
